4b5bd6e0f370e96fb3b8bae61e0d4722b2ea48c1
wrobbinz <wrobbinz@gmail.com>
2021-03-06 12:11:25 -0800

    listen for queue and remove background for image (#7)
    * listen for queue and remove background for image
    
    * Port docker changes
    
    Co-authored-by: Cody Pero <codypero@gmail.com>

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ dotenv.config();
 import { NestFactory } from '@nestjs/core';
 import { Logger } from '@nestjs/common';
 import { AppModule } from './app.module';
+import { UploadService } from './modules/upload';
 
 const { PORT } = process.env;
 
@@ -12,6 +13,10 @@ async function bootstrap() {
 
   /* Server Start */
   await app.listen(PORT);
+
+  /* Queue Start: Begin polling for model upload events */
+  const uploadService: UploadService = app.get(UploadService);
+  uploadService.listen();
 
   const logger = new Logger();
   logger.log(`Listening on port: ${PORT}`);
